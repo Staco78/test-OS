7df8da861a37937d4237106359fb2fbb124fefd6
Staco78 <contact@staco-dev.fr>
2021-11-24 16:34:17 +0100

    better disk read/write

--- src/include/fs.h
+++ src/include/fs.h
@@ -4,5 +4,5 @@
 #include "asm.h"
 #include "terminal.h"
 
-void readDisk(uint8 head, uint8 startSector, uint8 nbSector, uint16 cyl, uint8 *buff);
-void writeDisk(uint8 head, uint8 startSector, uint8 nbSector, uint32 cyl, uint8 *buff);
+void readDisk(uint32 address, uint16 nbSector, uint8 *buff);
+void writeDisk(uint32 address, uint16 nbSector, uint8 *buff);
--- src/kernel/fs.c
+++ src/kernel/fs.c
@@ -20,52 +20,68 @@ void outsw(uint16 port, void *addr, uint32 count)
     __asm__ volatile("sti");
 }
 
+void waitDiskReady()
+{
+    while (read_port(0x1F7) & 0b10000000)
+    {
+        ;
+    }
+}
+
 void fs_start()
 {
+
     uint8 buff[512];
+
+    for (uint32 i = 0; i < 512; i++)
+    {
+        buff[i] = 0x11;
+    }
+
+    writeDisk(0, 1, buff);
+
+    waitDiskReady();
+
+    uint8 buff2[512];
+
+    readDisk(0, 1, buff2);
 
     for (uint16 i = 0; i < 512; i++)
     {
-        buff[i] = i;
+        print8bitHex(buff2[i]);
+        if (i % 2 != 0)
+            printChar(' ');
     }
-
-    writeDisk(0, 1, 1, 0, buff);
 }
 
-void readDisk(uint8 head, uint8 startSector, uint8 nbSector, uint16 cyl, uint8 *buff)
+void readDisk(uint32 address, uint16 nbSector, uint8 *buff)
 {
-    write_port(0x1F6, head & 0b00001111);
+    write_port(0x1F6, (address >> 24) | 0b11100000);
     write_port(0x1F2, nbSector);
-    write_port(0x1F3, startSector);
-    write_port(0x1F4, (uint8)cyl);
-    write_port(0x1F5, (uint8)cyl >> 8);
+    write_port(0x1F3, (uint8)address);
+    write_port(0x1F4, (uint8)address >> 8);
+    write_port(0x1F5, (uint8)address >> 16);
     write_port(0x1F7, 0x20);
 
     while (!(read_port(0x1F7) & 8))
     {
     }
 
-    printBin(read_port(0x1F7));
-    printLn();
-
     insw(0x1F0, buff, 256);
 }
 
-void writeDisk(uint8 head, uint8 startSector, uint8 nbSector, uint32 cyl, uint8 *buff)
+void writeDisk(uint32 address, uint16 nbSector, uint8 *buff)
 {
-    write_port(0x1F6, head & 0b00001111);
+    write_port(0x1F6, (address >> 24) | 0b11100000);
     write_port(0x1F2, nbSector);
-    write_port(0x1F3, startSector);
-    write_port(0x1F4, (uint8)cyl);
-    write_port(0x1F5, (uint8)cyl >> 8);
+    write_port(0x1F3, (uint8)address);
+    write_port(0x1F4, (uint8)address >> 8);
+    write_port(0x1F5, (uint8)address >> 16);
     write_port(0x1F7, 0x30);
 
     while (!(read_port(0x1F7) & 8))
     {
     }
-
-    printBin(read_port(0x1F7));
-    printLn();
 
     outsw(0x1F0, buff, 256);
 }
